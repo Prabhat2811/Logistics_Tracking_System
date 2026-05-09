1a67dff1add0790893286d8065a8d14531c6e5a8
Prabhat Ranjan <ranjanprabhat.c@gmail.com>
2026-05-09 22:03:10 +0530

    Initial Eclipse Commit.

---
+++ Logistics_Tracking_System/src/main/java/jsp/tracking/LogisticsTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package jsp.tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ Logistics_Tracking_System/src/test/java/jsp/tracking/LogisticsTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jsp.tracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsTrackingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
